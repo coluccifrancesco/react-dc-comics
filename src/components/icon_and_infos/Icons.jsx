19ffc1fc5e2b04661dbeef91e482043a7f5c4808
coluccifrancesco <coluccif2002@gmail.com>
2025-07-15 00:46:19 +0200

    Put icons in blue banner

--- src/components/icon_and_infos/Icons.jsx
+++ src/components/icon_and_infos/Icons.jsx
@@ -1,7 +1,30 @@
 export default function Icons(){
+
+    const infoItems = [
+        {imgSrc:'../../../public/buy-comics-digital-comics.png', text:'DIGITAL COMICS', href:'#'},
+        {imgSrc:'../../../public/buy-comics-merchandise.png', text:'DC MERCHANDISE', href:'#'},
+        {imgSrc:'../../../public/buy-comics-subscriptions.png', text:'SUBSCRIPTION', href:'#'},
+        {imgSrc:'../../../public/buy-comics-shop-locator.png', text:'COMIC SHOP LOCATOR', href:'#'},
+        {imgSrc:'../../../public/buy-dc-power-visa.svg', text:'DC POWER VISA', href:'#'}
+    ];
+
+    const columns = [];
+
+    infoItems.forEach((item)=>{
+        columns.push(
+            <div className="col m-4 d-flex justify-content-between align-items-center" key={item.text}>
+                <img src={item.imgSrc} alt={item.text} />
+                <a href={item.href}>{item.text}</a>
+            </div>
+        );
+    });
+
+
     return(
-        <div className='infos'>
-            <h1>Icons goes here</h1>
+        <div className='infos d-flex flex-column justify-content-between align-items-center pt-4 pb-4'>
+            <div className="row">
+                {columns}
+            </div>
         </div>
     )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,7 +4,9 @@ import Icons from '../icon_and_infos/Icons'
 export default function Main(){
     return(
         <main>
-            <h2>-- Content goes here --</h2>
+            <div className='container-sm container-md container-lg container-xl d-flex flex-column justify-content-center align-items-center'>
+                <h2>-- Content goes here --</h2>
+            </div>
             <Icons />
         </main>
     )
