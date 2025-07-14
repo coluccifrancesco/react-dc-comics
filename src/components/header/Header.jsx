12d6244c97d3d4a130bb0059f0afb596882a37b4
coluccifrancesco <coluccif2002@gmail.com>
2025-07-14 17:53:55 +0200

    Implement elements in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import './header.css';
+import logo from '../../../public/dc-logo.png';
 
 export default function Header (){
     return(
         <header>
-            <h1>Ciao! Magari Header funzioni</h1>
+            <div>
+                <img src={logo} alt='logo' />
+                <ul>
+                    <a>Charachters</a>
+                    <a>Comics</a>
+                    <a>Movies</a>
+                    <a>TV</a>
+                    <a>Games</a>
+                    <a>Collectibles</a>
+                    <a>Videos</a>
+                    <a>Fans</a>
+                    <a>News</a>
+                    <a>Shop</a>
+                </ul>
+            </div>
         </header>
     )
 }
