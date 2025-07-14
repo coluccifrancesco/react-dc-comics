073f17fcbe030dee78f82fc27b197c27788ecd92
coluccifrancesco <coluccif2002@gmail.com>
2025-07-14 22:57:13 +0200

    Finish navbar with elements and  hover effects

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,39 @@ import logo from '../../../public/dc-logo.png';
 export default function Header (){
     return(
         <header>
-            <div>
-                <img src={logo} alt='logo' />
-                <ul>
-                    <a>Charachters</a>
-                    <a>Comics</a>
-                    <a>Movies</a>
-                    <a>TV</a>
-                    <a>Games</a>
-                    <a>Collectibles</a>
-                    <a>Videos</a>
-                    <a>Fans</a>
-                    <a>News</a>
-                    <a>Shop</a>
+            <nav className='container-sm container-md container-lg container-xl d-flex justify-content-between align-items-center'>
+                <img src={logo} alt='logo' className='mt-4 mb-4' />
+                
+                <ul className='d-flex justify-content-between align-items-center mb-0 '>
+                    <li>
+                        <a>Charachters</a>
+                    </li>
+                    <li>
+                        <a>Comics</a>
+                    </li>
+                    <li>
+                        <a>Movies</a>
+                    </li>
+                    <li>
+                        <a>TV</a>
+                    </li>
+                    <li>
+                        <a>Games</a>
+                    </li>
+                    <li>
+                        <a>Collectibles</a>
+                    </li>
+                    <li>
+                        <a>Videos</a>
+                    </li>
+                    <li>
+                        <a>News</a>
+                    </li>
+                    <li>
+                        <a>Shop</a>
+                    </li>
                 </ul>
-            </div>
+            </nav>
         </header>
     )
 }
