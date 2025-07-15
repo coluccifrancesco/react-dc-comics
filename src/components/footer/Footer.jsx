13906c135381eb1f17a93ceead7ac594881af6f6
coluccifrancesco <coluccif2002@gmail.com>
2025-07-15 12:44:33 +0200

    iterate and links done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,107 @@ import './footer.css';
 export default function Footer (){
     return(
         <footer>
-            <h1>Ciao! Magari Footer funzioni</h1>
+            <div className='row container-sm container-md container-lg container-xl pt-4 pb-4 mt-0 mb-0 ms-auto me-auto'>
+                <div className='col-6 row  d-flex justify-content-between'>
+                    <ul className='col col-4'>
+                        <li>
+                            <h4>DC COMICS</h4>
+                        </li>
+                        <li>
+                            <a>Characters</a>
+                        </li>
+                        <li>
+                            <a>Comics</a>
+                        </li>
+                        <li>
+                            <a>Movies</a>
+                        </li>
+                        <li>
+                            <a>TV</a>
+                        </li>
+                        <li>
+                            <a>Games</a>
+                        </li>
+                        <li>
+                            <a>Videos</a>
+                        </li>
+                        <li>
+                            <a>News</a>
+                        </li>
+                        <li>
+                            <h4>SHOP</h4>
+                        </li>
+                        <li>
+                            <a>Shop DC</a>
+                        </li>
+                        <li>
+                            <a>Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                    <ul className='col col-4'>
+                        <li>
+                            <h4>DC</h4>
+                        </li>
+                        <li>
+                            <a>Terms of Use</a>
+                        </li>
+                        <li>
+                            <a>Privacy policy (New)</a>
+                        </li>
+                        <li>
+                            <a>Ad Choices</a>
+                        </li>
+                        <li>
+                            <a>Advertising</a>
+                        </li>
+                        <li>
+                            <a>Jobs</a>
+                        </li>
+                        <li>
+                            <a>Subscriptions</a>
+                        </li>
+                        <li>
+                            <a>Talent Workshops</a>
+                        </li>
+                        <li>
+                            <a>CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a>Ratings</a>
+                        </li>
+                        <li>
+                            <a>Shop Help</a>
+                        </li>
+                        <li>
+                            <a>Contact Us</a>
+                        </li>
+                    </ul>
+                    <ul className='col col-4'>
+                        <li>
+                            <h4>SITES</h4>
+                        </li>
+                        <li>
+                            <a>DC</a>
+                        </li>
+                        <li>
+                            <a>MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a>DC Kids</a>
+                        </li>
+                        <li>
+                            <a>DC Universe</a>
+                        </li>
+                        <li>
+                            <a>DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div className='col-6 d-flex justify-content-center align-items-center'>
+                    <img src='../../../public/dc-logo-bg.png' className='logo' />
+                </div>
+            </div>
         </footer>
     )
 }
--- src/components/icon_and_infos/Icons.jsx
+++ src/components/icon_and_infos/Icons.jsx
@@ -1,29 +1,24 @@
+const infoItems = [
+    {imgSrc:'../../../public/buy-comics-digital-comics.png', alt:'DIGITAL COMICS', text:'DIGITAL COMICS', href:'#', key:1},
+    {imgSrc:'../../../public/buy-comics-merchandise.png', alt:'DC MERCHANDISE', text:'DC MERCHANDISE', href:'#', key:2},
+    {imgSrc:'../../../public/buy-comics-subscriptions.png', alt:'SUBSCRIPTION', text:'SUBSCRIPTION', href:'#', key:3},
+    {imgSrc:'../../../public/buy-comics-shop-locator.png', alt:'COMIC SHOP LOCATOR', text:'COMIC SHOP LOCATOR', href:'#', key:4},
+    {imgSrc:'../../../public/buy-dc-power-visa.svg', alt:'DC POWER VISA', text:'DC POWER VISA', href:'#', key:5}
+];
+
 export default function Icons(){
-
-    const infoItems = [
-        {imgSrc:'../../../public/buy-comics-digital-comics.png', text:'DIGITAL COMICS', href:'#'},
-        {imgSrc:'../../../public/buy-comics-merchandise.png', text:'DC MERCHANDISE', href:'#'},
-        {imgSrc:'../../../public/buy-comics-subscriptions.png', text:'SUBSCRIPTION', href:'#'},
-        {imgSrc:'../../../public/buy-comics-shop-locator.png', text:'COMIC SHOP LOCATOR', href:'#'},
-        {imgSrc:'../../../public/buy-dc-power-visa.svg', text:'DC POWER VISA', href:'#'}
-    ];
-
-    const columns = [];
-
-    infoItems.forEach((item)=>{
-        columns.push(
-            <div className="col m-4 d-flex justify-content-between align-items-center" key={item.text}>
-                <img src={item.imgSrc} alt={item.text} />
-                <a href={item.href}>{item.text}</a>
-            </div>
-        );
-    });
-
 
     return(
         <div className='infos d-flex flex-column justify-content-between align-items-center pt-4 pb-4'>
             <div className="row">
-                {columns}
+                
+                {infoItems.map((item) => {
+                    return <div className="col" key={item.key}>
+                        <img src={item.imgSrc} alt={item.alt} />
+                        <a href={item.href}>{item.text}</a>
+                    </div>
+                })}
+            
             </div>
         </div>
     )
