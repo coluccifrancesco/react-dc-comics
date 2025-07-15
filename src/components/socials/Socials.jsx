ff5569e89508824d09275e7a2bb868ba26060850
coluccifrancesco <coluccif2002@gmail.com>
2025-07-15 23:40:58 +0200

    adjust footer and add hover effects

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -3,9 +3,12 @@ import './socials.css';
 export default function Socials() {
     return(
         <div className='bg'>
-            <div className='container-sm container-md container-lg container-xl d-flex justify-content-between align-items-center pt-4 pb-4 socials'>
-                <button className='sign'>SIGN-UP NOW!</button>
-                <div className='d-flex justify-content-between align-items-center'>
+            <div className='row container-sm container-md container-lg container-xl d-flex justify-content-between align-items-center py-4 mx-auto socials'>
+                <div className='col col-3'>
+                    <button className='sign'>SIGN-UP NOW!</button>
+                </div>
+                
+                <div className='col-5 d-flex justify-content-between align-items-center gap-3'>
                     <h4>FOLLOW US</h4>
                     <img src='../../../public/footer-facebook.png' alt='facebook' />
                     <img src='../../../public/footer-twitter.png' alt='twitter' />
@@ -13,6 +16,7 @@ export default function Socials() {
                     <img src='../../../public/footer-pinterest.png' alt='pinterest' />
                     <img src='../../../public/footer-periscope.png' alt='periscope' />
                 </div>
+            
             </div>
         </div>
     )
